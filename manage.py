8d988f524664a51ee752d8a5023bdc7b3e8a0344
Namik Karovic <namik.karovic@gmail.com>
2017-01-23 14:19:19 +0100

    multi threaded server prototype
    probably should be made to work with the Manager

--- manage.py
+++ manage.py
@@ -3,12 +3,18 @@ import os
 from app import create_app
 from flask_script import Manager, Shell
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
-manager = Manager(app)
+app.run(threaded=True)
 
-def make_shell_context():
-    return dict(app=app)
+# couldnt get it to work with the rest of the code
+# I'm just gonna comment it out
+# seems to work fine without it...
 
-manager.add_command("shell", Shell(make_context=make_shell_context))
+# manager = Manager(app)
 
-if __name__ == '__main__':
-    manager.run()
+# def make_shell_context():
+#     return dict(app=app)
+
+# manager.add_command("shell", Shell(make_context=make_shell_context))
+
+# if __name__ == '__main__':
+#     manager.run()
